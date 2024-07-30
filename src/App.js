9ca86dbab7a83372f44fa95445a34323640a666b
조하민 <chohamin0611@gmail.com>
2024-07-30 10:29:33 +0900

    FIX :: 유틸리티 버튼 위치 수정

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='header'>
-        <ul>
-          <li className='coming-soon'>오픈예정</li>
-          <li className='coming-soon'>펀딩 +</li>
-          <li className='coming-soon'>프리오더</li>
-          <li className='coming-soon'>스토어</li>
-          <li className='coming-soon'>더보기</li>
-        </ul>
-      </div>
+      <header>
+        <div className='header-left'>
+          <ul>
+            <li className='coming-soon'>오픈예정</li>
+            <li className='coming-soon'>펀딩 +</li>
+            <li className='coming-soon'>프리오더</li>
+            <li className='coming-soon'>스토어</li>
+            <li><button className='coming-soon'>더보기</button></li>
+          </ul>
+        </div>
+        <div className='util'>
+          <button className='header-right'>로그인</button>
+          <button className='header-right'>회원가입</button>
+          <div className='project-made'>프로젝트 만들기</div>
+        </div>
+      </header>
     </div>
   );
 }
